7c896abc659dde2cf2edc96ec98f190bf273cf71
SachinBalagam <balagamsachin337@gmail.com>
2023-07-28 16:19:39 +0000

    Final Commit

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,1 +1,49 @@
 // Write your code here
+import {Component} from 'react'
+import DenominationItem from '../DenominationItem'
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {amount: 2000}
+
+  onClickAmount = value => {
+    this.setState(prevState => ({amount: prevState.amount - value}))
+  }
+
+  render() {
+    const {denominationsList} = this.props
+    const {amount} = this.state
+    return (
+      <div className="bg-container">
+        <div className="card">
+          <div className="name-container">
+            <div className="initial-div">
+              <p className="initial">S</p>
+            </div>
+            <h1 className="name">Sarah Williams</h1>
+          </div>
+          <div className="balance-container">
+            <p className="your-balance">Your Balance</p>
+            <div>
+              <p className="amount">{amount}</p>
+              <p className="in-rupees">In Rupees</p>
+            </div>
+          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denomination-container">
+            {denominationsList.map(each => (
+              <DenominationItem
+                denominationsList={each}
+                key={each.id}
+                onClickAmount={this.onClickAmount}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,1 +1,21 @@
 // Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationsList, onClickAmount} = props
+  const {value} = denominationsList
+
+  const onAmountClick = () => {
+    onClickAmount(value)
+  }
+
+  return (
+    <li className="each-denomination">
+      <button type="button" className="button" onClick={onAmountClick}>
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
